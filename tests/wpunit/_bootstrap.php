c7424ba9b49d0077db2f6370daa5a726fb0380c7
Brian Henry <BrianHenryIE@gmail.com>
2020-05-26 23:10:48 -0700

    Moved autolading in test bootstrap only where it's needed

--- tests/wpunit/_bootstrap.php
+++ tests/wpunit/_bootstrap.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * PHPUnit bootstrap file for WP_Mock.
+ * PHPUnit bootstrap file for wpunit tests. Since the plugin will not be otherwise autoloaded.
  *
  * @package           Plugin_Package_Name
  */
 
 require_once $plugin_root_dir . '/autoload.php';
-
-WP_Mock::bootstrap();
