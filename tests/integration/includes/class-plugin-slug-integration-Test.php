666b8bbe02155cef20e6facb06161e518fb0121e
Brian Henry <BrianHenryIE@gmail.com>
2020-11-30 09:45:11 -0800

    Update class-plugin-slug-integration-Test.php

--- tests/integration/includes/class-plugin-slug-integration-Test.php
+++ tests/integration/includes/class-plugin-slug-integration-Test.php
@@ -185,5 +185,45 @@ class Plugin_Package_Name_Integration_Test extends \Codeception\TestCase\WPTestC
 
 		$this->assertEquals( $method_name, $hooked_method, "Unexpected method name for $class_type class hooked to $action_name" );
 
+		$action_name       = 'admin_init';
+		$expected_priority = 10;
+
+		$class_type = Admin::class;
+		$method_name = 'check_required_plugin_private_uploads';
+
+		$function_is_hooked = $this->is_function_hooked_on_action( $class_type, $method_name, $action_name, $expected_priority );
+
+		$this->assertNotFalse( $function_is_hooked );
+	}
+
+
+	protected function is_function_hooked_on_action( $class_type, $method_name, $action_name, $expected_priority = 10 ) {
+
+		global $wp_filter;
+
+		$this->assertArrayHasKey( $action_name, $wp_filter, "$method_name definitely not hooked to $action_name" );
+
+		$actions_hooked = $wp_filter[ $action_name ];
+
+		$this->assertArrayHasKey( $expected_priority, $actions_hooked, "$method_name definitely not hooked to $action_name priority $expected_priority" );
+
+		$hooked_method = null;
+		foreach ( $actions_hooked[ $expected_priority ] as $action ) {
+			$action_function = $action['function'];
+			if ( is_array( $action_function ) ) {
+				if ( $action_function[0] instanceof $class_type ) {
+					if( $method_name === $action_function[1] ) {
+						$hooked_method = $action_function[1];
+						break;
+					}
+				}
+			}
+		}
+
+		$this->assertNotNull( $hooked_method, "No methods on an instance of $class_type hooked to $action_name" );
+
+		$this->assertEquals( $method_name, $hooked_method, "Unexpected method name for $class_type class hooked to $action_name" );
+
+		return true;
 	}
 }
