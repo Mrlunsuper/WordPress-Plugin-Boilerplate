73f84ff9efa684bf6adf3173265369c62720cd01
Brian Henry <BrianHenryIE@gmail.com>
2021-09-25 07:16:48 -0700

    spelling

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 /**
- * Runs after WordPress has bee initialised (after plugins are loaded) and before tests are run.
+ * Runs after WordPress has been initialised (after plugins are loaded) and before tests are run.
  */
