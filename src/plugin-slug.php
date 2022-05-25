01f1f962a853a90e2c7b38be435439f6e9931f61
Brian Henry <BrianHenryIE@gmail.com>
2022-05-25 13:03:02 -0700

    define PLUGIN_NAME_PATH and PLUGIN_NAME_URL
    This will probably cause tests to fail for now

--- src/plugin-slug.php
+++ src/plugin-slug.php
@@ -44,8 +44,9 @@ require_once plugin_dir_path( __FILE__ ) . 'autoload.php';
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'PLUGIN_NAME_VERSION', '1.0.0' );
-
 define( 'PLUGIN_NAME_BASENAME', plugin_basename( __FILE__ ) );
+define( 'PLUGIN_NAME_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_NAME_URL', trailingslashit( plugins_url( __DIR__ ) ) );
 
 register_activation_hook( __FILE__, array( Activator::class, 'activate' ) );
 register_deactivation_hook( __FILE__, array( Deactivator::class, 'deactivate' ) );
