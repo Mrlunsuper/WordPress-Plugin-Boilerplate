a4b78df286b1a94548149d4f80a07ebb44427f22
Brian Henry <BrianHenryIE@gmail.com>
2022-10-27 21:40:59 -0700

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -13,7 +13,7 @@
 
 namespace Plugin_Package_Name;
 
-use Plugin_Package_Name\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
+use Plugin_Package_Name\Alley_Interactive\Autoloader\Autoloader;
 
 $class_map_files = array(
     __DIR__ . '/autoload-classmap.php',
@@ -40,6 +40,7 @@ unset( $class_map_files, $class_map_file, $class_map );
 // Load strauss classes after autoload-classmap.php so classes can be substituted.
 require_once __DIR__ . '/vendor-prefixed/autoload.php';
 
-$wpcs_autoloader = new WP_Namespace_Autoloader( array( 'classes_dir' => array( 'src' ) ) );
-$wpcs_autoloader->init();
-
+Autoloader::generate(
+	'BrianHenryIE\\WC_Set_Gateway_By_URL',
+	__DIR__ . '/src',
+)->register();
